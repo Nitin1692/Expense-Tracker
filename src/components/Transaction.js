305425ff00a289d77eddeb874b5a492464473d77
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2022-12-28 16:51:16 +0530

    Balance and Transaction is working fine

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 function Transaction({ transaction}) {
     const sign = transaction.amount < 0 ? '-' : '+';
+    
     return (
         <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
                 {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
